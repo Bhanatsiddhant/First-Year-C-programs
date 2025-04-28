29176d69025751e250756b3661ac87961c56e79e
Siddhant Bhanat <139568851+Bhanatsiddhant@users.noreply.github.com>
2025-04-28 21:34:56 +0530

    Add files via upload

---
+++ else-if.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+  int marks;
+  printf("Enter obtained marks:\t");
+  scanf("%d", &marks);
+  printf("\n Your Grade is:\t");
+  if (marks > 100 || marks < 0) {
+    printf("The Entered marks are not valid.Please enter valid marks.");
+  } else if (marks >= 80) {
+    printf("A grade");
+  } else if (marks >= 70) {
+    printf("B grade");
+  } else if (marks >= 60) {
+    printf("C grade");
+  } else if (marks >= 50) {
+    printf("D grade");
+  } else if (marks >= 40) {
+    printf("E grade");
+  } else {
+    printf("Failed");
+  }
+  return 0;
+}
