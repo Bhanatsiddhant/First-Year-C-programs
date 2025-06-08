3c0286f93d841b6561d463f3e4962e93235bbecd
Bhanatsiddhant <bhanatsiddhant@gmail.com>
2025-06-08 15:06:44 +0530

    code

--- Addition.c
+++ Addition.c
@@ -1,14 +1,13 @@
 /*PRN: 24030331372901
  Name: Siddhant Chandrakant Bhanat
  Title: Addition
- Date: 
+ Date:
 */
 
-#include<stdio.h>
-int main()
-{
-	int num1 = 2,num2 = 3,sum;
-	sum = num1+num2;
-	printf("The sum of %d and %d is %d",num1,num2,sum);
-	return 0;
+#include <stdio.h>
+int main() {
+  int num1 = 2, num2 = 3, sum;
+  sum = num1 + num2;
+  printf("The sum of %d and %d is %d", num1, num2, sum);
+  return 0;
 }
